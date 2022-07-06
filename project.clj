f5911d055059f5bbcc0e79c1226c843376fb5668
David Frese <david.frese@active-group.de>
2022-07-06 19:11:17 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.active-group/reacl-c-reacl "0.1.0-SNAPSHOT"
+(defproject de.active-group/reacl-c-reacl "0.1.0"
   :description "Library for interop between Reacl-c and Reacl."
   :url "http://github.com/active-group/reacl-c-reacl"
   
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [org.clojure/clojurescript "1.10.773" :scope "provided"]
                  [de.active-group/active-clojure "0.36.0"]
-                 [de.active-group/reacl-c "0.11.0-SNAPSHOT"]
+                 [de.active-group/reacl-c "0.11.0"]
                  [reacl "2.2.9"]
                  [de.active-group/cljs-async "2.0.0"]]
 
